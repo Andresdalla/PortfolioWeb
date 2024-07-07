bc39eb28f1af4b1bf1a62ba927a76cc9d3d7620d
Andres Dalla Rizza <andresdallarizza1081@gmail.com>
2024-07-07 13:19:44 -0300

    mi trabajo

--- src/components/MiTrabajo.jsx
+++ src/components/MiTrabajo.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import Logisim from '../assets/trabajos/logisim.png' 
 
 const MiTrabajo = () => {
   return (
     <div name = 'trabajos' className='w-full md:h-screen bg-[#fffffd]'>
-        <div className='max-w.[1000px] mx-auto p-4 flex flex col justify-center w-full h-full'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#209d5c]'>Proyectos</p>
                 <p className='py-6'>Mira los trabajos que hice paaaa</p>
             </div>
 
-            <div className='grid sm:gird-cols-2 md:grid-cols-3 gap-4'>
+            <div style={{backgroundImage: `url(${Logisim})`}} 
+            className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
                 <div className='shadow-lg shadow-[#fffffda9] group container rounded-md
                 flex justify-center items-center mx-auto content-div'>
                     {/* efecto hover */}
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,7 +14,7 @@ const Skills = () => {
   return (
     <div name='skills' className='w-full h-screen bg-[#fffffd]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className=''>
+        <div>
           <p className='text-4xl font-bold inline border-b-4 border-[#209d5c]'>
             Skills</p>
           <p className='py-4'>Estas son las tecnologías con las que trabaje</p>
