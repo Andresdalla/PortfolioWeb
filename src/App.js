7452ffd70a7c7b9ef475746818303f5aed0d4eca
Andres Dalla Rizza <andresdallarizza1081@gmail.com>
2024-07-08 15:43:59 -0300

    no funca

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import SobreMi from "./components/SobreMi";
 import Skills from "./components/Skills";
 import MiTrabajo from "./components/MiTrabajo";
 import Contacto from "./components/Contacto"
-
+import Aseternity from "./assets/Aseternity";
 function App() {
   return (
     <div>
       <Navbar/>
       <Home/>
-      <SobreMi/>
+      {/* <SobreMi/> */}
+      <Aseternity/>
       <div name='skills' className='w-full h-screen bg-[#fffffd]' style={{ marginBottom: '200px' }}>
         <Skills/>
       </div>
